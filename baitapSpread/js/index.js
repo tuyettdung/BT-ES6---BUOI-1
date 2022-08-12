74ff6fb9af725e06f9d34a0ac5b5377beac897a5
DungTu <tutuyettdung@gmail.com>
2022-08-12 15:58:14 +0700

    edit Spread

--- baitapSpread/js/index.js
+++ baitapSpread/js/index.js
@@ -1,4 +1,10 @@
-// let mang = "fggh";
-// let char = [...mang];
-// console.log(char);
+let chuoiKyTu = document.querySelector('.heading').innerHTML.split('');
+let chuoiMoi = [...chuoiKyTu];
+let html = '';
+for(let kyTu of chuoiMoi){
+   html += `
+              <span>${kyTu}</span>
+           `
+    document.querySelector('.heading').innerHTML= html;
+}
 
